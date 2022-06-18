0cb05f95f1c60e6014351cd68afe314043857052
Anwar Sh <alvaro.sh03@gmail.com>
2022-06-18 23:57:33 +0500

    Upload part1 courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,66 @@
+const App = () => {
+  //const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course['name']}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.course.parts[0]['exercises'] + props.course.parts[1]['exercises'] + props.course.parts[2]['exercises']}</p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part['name']} {props.part['exercises']}
+      </p>
+    </>
+  )
+}
+
+export default App
